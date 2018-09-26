34dafa01bffcb3407629b5aa3a099300f653e8fd
Alexander Zyurkalov <31346594+Alexander-Zyurkalov@users.noreply.github.com>
2018-09-26 21:45:19 +0300

    username and password

--- src/main/com/lynda/Main.java
+++ src/main/com/lynda/Main.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "";
+    private static final String USERNAME = "lyndacom";
+    private static final String PASSWORD = "password";
     private static final String CONN_STRING =
             "jdbc:mysql://localhost/explorecalifornia?autoReconnect=true&useSSL=false";
     public static void main( String[] args ) throws SQLException{
